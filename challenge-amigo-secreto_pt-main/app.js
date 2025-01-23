69e681d5c5c3d10f2a6aa53750f021a74af0219f
Joao paulo <joaopaulomarques654@gmail.com>
2025-01-23 19:46:24 -0400

    Adicionando funcionalidade de sorteio de amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -13,7 +13,7 @@ function adicionarAmigo() {
     atualizarLista();
 
     document.querySelector('input').value = "";
-    
+
     console.log(amigos);
 }
 
@@ -29,5 +29,23 @@ function atualizarLista() {
         listaAmigos.appendChild(li);
     });
 }
+
+function sortearAmigo() {
+    
+    if (amigos.length === 0) {
+        alert("A lista de amigos está vazia! Adicione alguns nomes primeiro.");
+        return; 
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultadoElemento = document.getElementById('resultado');
+    resultadoElemento.innerHTML = `<li> Amigo sorteado: <strong>${amigoSorteado}</strong></li>`;
+
+    console.log(`Amigo sorteado: ${amigoSorteado}`);
+}
+
 
 
